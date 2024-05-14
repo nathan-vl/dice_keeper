78e7752328d0f790d20a530518e46779966d36ae
Guilherme Euller <guilhermeeuller10@gmail.com>
2024-05-14 14:10:53 -0300

    Separando a tela em classes

---
+++ lib/list_player.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ListPlayer extends StatelessWidget {
+  const ListPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return _ListPlayerState();
+  }
+}
+
+class _ListPlayerState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Padding(
+            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: CardCharacter())
+      ],
+    );
+  }
+}
+
+class CardCharacter extends StatelessWidget {
+  const CardCharacter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return _CardCharacter();
+  }
+}
+
+class _CardCharacter extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {},
+        title: const Text('Nome do Personagem'),
+        subtitle: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nome da Sala'),
+            Text('Lv 10'),
+          ],
+        ),
+        trailing: Image.asset("assets/splash_1152.png"),
+      ),
+    );
+  }
+}
