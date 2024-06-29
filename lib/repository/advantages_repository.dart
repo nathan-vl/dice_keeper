aabf8f84c76b64deb2e3deb304c487d71e11e50d
Guilherme Euller <guilhermeeuller10@gmail.com>
2024-06-29 08:37:38 -0300

    Fix: repository advantage/disadvantage

--- lib/repository/advantages_repository.dart
+++ lib/repository/advantages_repository.dart
@@ -1,15 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dice_keeper/models/skill.dart';
+import 'package:dice_keeper/models/advantage.dart';
 
 final _db = FirebaseFirestore.instance;
 
 class AdvantagesRepository {
-  static Future<List<Skill>> get() async {
+  static Future<List<Advantage>> get() async {
     final document =
         await _db.collection("rpg_properties").doc("properties").get();
     final data = document.data()!;
     final items = data["vantagens"];
-    final advantages = items.map((advantage) => Skill.fromMap(advantage));
-    return List<Skill>.from(advantages.toList() as List);
+    final advantages = items.map((advantage) => Advantage.fromMap(advantage));
+    return List<Advantage>.from(advantages.toList() as List);
   }
 }
--- lib/repository/disadvantages_repository.dart
+++ lib/repository/disadvantages_repository.dart
@@ -1,15 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dice_keeper/models/skill.dart';
+import 'package:dice_keeper/models/disadvantage.dart';
 
 final _db = FirebaseFirestore.instance;
 
 class DisadvantagesRepository {
-  static Future<List<Skill>> get() async {
+  static Future<List<Disadvantage>> get() async {
     final document =
         await _db.collection("rpg_properties").doc("properties").get();
     final data = document.data()!;
     final items = data["desvantagens"];
-    final disadvantages = items.map((disadvantage) => Skill.fromMap(disadvantage));
-    return List<Skill>.from(disadvantages.toList() as List);
+    final disadvantages = items.map((disadvantage) => Disadvantage.fromMap(disadvantage));
+    return List<Disadvantage>.from(disadvantages.toList() as List);
   }
 }
